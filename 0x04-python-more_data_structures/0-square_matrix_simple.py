9a5399bcdad062c74a63376cc4d5797be82d7d76
Raydee97 <blvckree@gmail.com>
2022-06-08 11:36:22 -0700

    More data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
+"""
+computes the square value of all integers of a matrix.
+
+Args: 
+    matrix: 2D list to perform operation on
+
+Prints:
+    same size and square of all value of a matrix
+"""
 
 
 def square_matrix_simple(matrix=[]):
     square_list = []
     for items in matrix:
-        square_list(list(map(lambda X: X ** 2, items)))
+        square_list.append(list(map(lambda X: X ** 2, items)))
         print(square_list)
