dfdc263cd9d1f27f4a43d744a8ee637c32fa368e
Raydee97 <blvckree@gmail.com>
2022-06-08 10:57:05 -0700

    More data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@
 
 
 def square_matrix_simple(matrix=[]):
-    list(map(lambda matrix: list(map(lambda x: x**2, matrix)), matrix))
+    list(map(lambda matrix: list(map(lambda x: x**2, range(matrix))), matrix))
