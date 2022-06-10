b6a7b1c21f3e2cb921a440762700786d470b7ab5
Raydee97 <blvckree@gmail.com>
2022-06-10 08:27:26 -0700

    More data structures

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    """
+    returns a key with the biggest integer value.
+
+    """
+    if a_dictionary is None:
+            return None
+    best = 0
+    best_key = None
+    for key, val in a_dictionary.items():
+            if val > best:
+                    best = val
+                    best_key = key
+    return best_key
---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    """
+     deletes a key in a dictionary.
+    
+    """
+    if a_dictionary.get(key):
+        del a_dictionary[key]
+    return a_dictionary
+
