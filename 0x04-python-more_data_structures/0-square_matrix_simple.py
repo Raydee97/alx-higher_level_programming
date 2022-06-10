3926720b7c5ae104588afc1b8b0e0725f4f9d1f4
Raydee97 <blvckree@gmail.com>
2022-06-10 10:32:49 -0700

    More data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
-"""
-computes the square value of all integers of a matrix.
-
-Args: 
-    matrix: 2D list to perform operation on
-
-Return:
-    same size and square of all value of a matrix
-"""
-
-
 def square_matrix_simple(matrix=[]):
-    new_list = []
-    for items in matrix:
-        new_list.append(list(map(lambda X: X ** 2, items)))
-        return new_list
+    """
+    computes the square value of all integers of a matrix.
+
+    """
+    return list(map(lambda newMatrix: list(map(lambda v: v*v, newMatrix)), matrix))
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,6 +2,7 @@
 def uniq_add(my_list=[]):
     """
     adds all unique integers in a list (only once for each integer).
+    
     """
     return sum(set(my_list))
 
